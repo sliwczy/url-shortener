8ec16fa246d9d7e964c1c090073678ff4e4b724a
Mateusz Sliwa <msliwa77@gmail.com>
2025-04-25 16:31:59 +0200

    fixed broken template

--- src/main/java/io/shortcut/controller/UrlEndpoint.java
+++ src/main/java/io/shortcut/controller/UrlEndpoint.java
@@ -1,22 +1,25 @@
 package io.shortcut.controller;
 
 import io.shortcut.service.UrlMappingService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
+@RequiredArgsConstructor
 public class UrlEndpoint {
 
     private final UrlMappingService urlService;
 
     @GetMapping
-    public String getUrlMappings() {
-
+    public Map<String, String> getUrlMappings() {
+        return Map.of();
     }
 
     @PostMapping
-    public String createUrl() {
-
+    public void createUrl() {
     }
 }
